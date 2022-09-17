22b7876e7c3513e69c8e191d5e6bcebff0c9e313
Rahul <rahulthor.081203@gmail.com>
2022-09-17 10:10:49 +0530

    Added comments to the UserInputCalculator.java

--- UserInputCalculator.java
+++ UserInputCalculator.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+import java.util.Scanner; //Importing the Scanner class for java to take user input
 public class UserInputCalculator {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc= new Scanner(System.in); // Creating an object of the class Scanner to take keyboard input
         System.out.println("Enter number 1: ");
-        float num1=sc.nextFloat();
+        float num1=sc.nextFloat();  // User input 1
         System.out.println("Enter number 2: ");
-        float num2=sc.nextFloat();
+        float num2=sc.nextFloat();  // User input 2
         
-        float sum=num1+num2;
+        float sum=num1+num2; // Add the inputs
         System.out.println("Sum is: "+sum);
         
     }
